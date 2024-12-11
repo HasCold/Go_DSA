f2a31a31c3c3baa3ba79ba2e630941d8f367cca8
Hasan <ha03330224926@gmail.com>
2024-12-11 21:17:45 +0500

    Add Find Duplicate(Slow Fast Pointer Approach)

--- Hashing_DS/Find_Duplicate/main.go
+++ Hashing_DS/Find_Duplicate/main.go
@@ -25,8 +25,47 @@ func findDuplicate(arr []int) int {
 
 }
 
+// Slow-Fast Pointer Approach
+// element :-  [3, 1, 3, 4, 2] ==>> Next Node
+// index :- 	0, 1, 2, 3, 4  ==>> Node
+
+// Imagine array as a linkedlist
+
+// Practically :- 0, 3, 4, 2, 3
+// T.C. := O(n)
+// S.C. := O(1)
+
+// 1. Slow Pointer => +1
+// 2. Fast Pointer => +2
+// Slow == Fast
+
+func slowFastApproach(arr []int) int {
+
+	slow := arr[0]
+	fast := arr[0]
+
+	for {
+		slow = arr[slow]      // Update with +1
+		fast = arr[arr[fast]] // Update with +2
+
+		if slow == fast {
+			break
+		}
+	}
+
+	slow = arr[0]
+	for slow != fast {
+		slow = arr[slow] // Update with +1
+		fast = arr[fast] // Update with +1
+	}
+
+	return slow
+}
+
 func main() {
 	arr := []int{3, 1, 3, 4, 2}
 
 	fmt.Println(findDuplicate(arr))
+
+	fmt.Println("The slow fast pointer approach :- ", slowFastApproach(arr))
 }
